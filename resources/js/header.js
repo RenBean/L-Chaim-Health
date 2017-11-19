879279ae8f44acbe74e693a30976b2dcd907ed24
Ari Dalton <ren_logic@hotmail.com>
2017-11-18 17:14:48 -0700

    Launch

---
+++ resources/js/header.js
@@ -0,0 +1,78 @@
+document.write(
+'		<div id="index"><strong><nav class="navbar navbar-expand-md navbar-light bg-light">'+
+'			<a class="navbar-brand" href="index.html"> <img src="resources/img/chalice.gif"'+
+'				width="30" height="40" class="d-inline-block align-top"><sub><span id="enlarge">&nbsp;&nbsp; L\'Chaim Health</span></sub>'+
+'			</a>'+
+'			<button class="navbar-toggler" type="button" data-toggle="collapse"'+
+'				data-target="#navbarSupportedContent"'+
+'				aria-controls="navbarSupportedContent" aria-expanded="false"'+
+'				aria-label="Toggle navigation">'+
+'				<span class="navbar-toggler-icon"></span>'+
+'			</button>'+
+'			<div class="collapse navbar-collapse" id="navbarSupportedContent">'+
+'				<ul class="navbar-nav mr-auto">'+
+'					<li class="nav-item"><a class="nav-link" href="classes.html">Classes</a>'+
+'					</li>'+
+'					<li class="nav-item dropdown">'+
+'						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown"'+
+'						role="button" data-toggle="dropdown" aria-haspopup="true"'+
+'						aria-expanded="false">Products</a>'+
+'						<div id="size-control" class="dropdown-menu" aria-labelledby="navbarDropdown"> '+
+'							<a class="nav-link disabled" href="#"><span class="grey">Joan\'s Inventory</span></a> '+ 
+'							<a class="dropdown-item" href="books.html">Books</a> '+
+'							<a class="dropdown-item" href="dvd.html">DVD\'s</a> '+
+'							<a class="dropdown-item" href="essentialOils.html">Essential Oils</a> '+
+'							<a class="dropdown-item" href="immuneSystemBuilders.html">Immune System Builders</a> '+
+'							<a class="dropdown-item" href="needakRebounders.html">Needak Rebounders</a>'+
+'							<div class="dropdown-divider"></div>'+
+'							<a class="nav-link disabled" href="#"><span class="grey">Global Healing Center</span></a>'+
+'							<a class="dropdown-item" href="http://www.globalhealingcenter.com/lung-cleanse-allertrex.html?a_aid=56f1d87290ca2&amp;a_bid=9c4a414f">Allertrex</a> '+
+'							<a class="dropdown-item" href="http://www.globalhealingcenter.com/nascent-iodine-detoxadine.html?a_aid=56f1d87290ca2&amp;a_bid=c2703dd3">Detoxadine</a> '+
+'							<a class="dropdown-item" href="http://www.globalhealingcenter.com/cleansing/livatrex-liver-support.html?a_aid=56f1d87290ca2&amp;a_bid=73df96d3">Livatrex</a> '+
+'							<a class="dropdown-item" href="http://www.globalhealingcenter.com/cleansing/oxy-powder.html?a_aid=56f1d87290ca2&amp;a_bid=fab9c387">Oxy-Powder</a> '+
+'							<a class="dropdown-item" href="http://www.globalhealingcenter.com/turmeric.html?a_aid=56f1d87290ca2&a_bid=5307c496">Turmeric</a> '+
+'							<a class="dropdown-item" href="http://www.globalhealingcenter.com/vegansafe-b-12.html?a_aid=56f1d87290ca2&amp;a_bid=e10112b6">VeganSafe B-12</a>'+
+'						</div></li>'+
+'											<li class="nav-item dropdown">'+
+'						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown"'+
+'						role="button" data-toggle="dropdown" aria-haspopup="true"'+
+'						aria-expanded="false">Articles</a>'+
+'						<div id="size-control" class="dropdown-menu" aria-labelledby="navbarDropdown"><strong>'+
+'							<a class="nav-link disabled" href="#">Healthy Habits</a> '+
+'							<a class="dropdown-item" href="diatomaceousEarth.html">Diatomaceous Earth</a> '+
+'							<a class="dropdown-item" href="healthThroughNutrition.html">Health Through Nutrition</a> '+
+'							<a class="dropdown-item" href="benefitsOfJuicing.html">Benefits of Juicing</a> '+
+'							<a class="dropdown-item" href="wholeFoodNutrition.html">Whole Food Nutrition</a>'+
+'							<a class="dropdown-item" href="improvingYourDiet.html">Improving Your Diet</a> '+
+'							<a class="dropdown-item" href="understandingDetoxification.html">Understanding Detoxification</a> '+
+'							<a class="dropdown-item" href="detoxification.html">Detoxification</a> '+
+'							<a class="dropdown-item" href="antidepressants.html">Antidepressants</a> '+
+'							<a class="dropdown-item" href="highBloodPressure.html">High Blood Pressure</a> '+
+'							<a class="dropdown-item" href="acid.html">ACID - the Trojan Horse</a> '+
+'							<a class="dropdown-item" href="emotionalHealing.html">Emotional Healing</a>'+
+'						</strong></div></li>'+
+'						<li class="nav-item dropdown">'+
+'						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown"'+
+'						role="button" data-toggle="dropdown" aria-haspopup="true"'+
+'						aria-expanded="false">Links</a>'+
+'						<div id="size-control" class="dropdown-menu" aria-labelledby="navbarDropdown">'+
+'							<a class="nav-link disabled" href="#">Resources</a> '+
+'							<a class="dropdown-item" href="http://ChooseLife.JuicePlus.com"><strong>ChooseLife.JuicePlus.com</strong></a><div id="center"><span id="shrink">You need more whole-food nutrition. Research shows that <span id="color-orange">Juice Plus+</span> delivers fruit and vegetable nutrition you need to maintain a healthy diet.</span></div> '+
+'							<a class="dropdown-item" href="http://YoungLiving.com/KLMFamily"><strong>YoungLiving.com/KLMFamily</strong></a><div id="center"><span id="shrink">Young Living succeeds by providing real wellness solutions, not side effects.<br>-Young Living Essential Oils-</span></div> '+
+'							<a class="dropdown-item" href="http://www.StandardProcess.com"><strong>www.StandardProcess.com</strong></a><div id="center"><span id="shrink">Achieve optimal health with Standard Process products.</span></div> '+
+'							<a class="dropdown-item" href="http://www.BrainAdvance.org"><strong>www.BrainAdvance.org</strong></a><div id="center"><span id="shrink">Special Education; Brain Injury Therapy, Autism Therapy, Traumatic Brain Injury, & Emotional Trauma Clearing.</span></div> '+
+'						</div></li>'+
+'					<li class="nav-item"><a class="nav-link" href="contact.html">Contact</a>'+
+'					</li>'+
+'				</ul>'+
+'				<span class="nav-item active"><a class="nav-link"'+
+'						href="#"><span id="dateStamp"></span><span class="sr-only">(current)</span></a>'+
+'				</span>'+
+'				<!-- <form class="form-inline my-2 my-lg-0">'+
+'					<input class="form-control mr-sm-2" type="search" '+
+'						placeholder="Search" aria-label="Search"\'>'+
+'					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>'+
+'				</form> -->'+
+'			</div></div>'+
+'		</nav>'+ 
+'');
